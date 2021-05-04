1fc3cd00678677948e3154c8805e4583d7c80df0
Mykhail Bobrykov <gelios238@gmail.com>
2021-05-04 15:38:09 +0300

    valideate RegisterForm

--- react-node-chat/src/modules/RegisterForm/components/RegisterForm.jsx
+++ react-node-chat/src/modules/RegisterForm/components/RegisterForm.jsx
@@ -4,7 +4,9 @@ import {Link} from 'react-router-dom';
 
 import { ContainerEnteryForm } from "../../../components";
 
-const RegisterForm = () => {
+const RegisterForm = props => {
+	const {values, touched, errors, handleChange, handleBlur, handleSubmit} = props;
+
 	 return (
 		<div className='auth'>
 			<h2>Регистрация</h2>
@@ -12,61 +14,70 @@ const RegisterForm = () => {
 				<Form
 					name=''
 					className=''
+					onSubmit={handleSubmit}
 				>
 					<Form.Item
 						name='email'
-						rules={[
-							{
-								required: true,
-								message: 'Пожалуйста введите свое email'
-							}
-						]}
-						validateStatus="success" 
+						help={touched.email && errors.email}
+						validateStatus={!touched.email ? 'success' : errors.email ? 'error' : 'success' }
 						hasFeedback
 					>
-						<Input prefix={<MailOutlined className='auth__icon-input'/>} placeholder="E-email" size='large'/>
+						<Input 
+							prefix={<MailOutlined className='auth__icon-input'/>} 
+							placeholder="E-email" 
+							size='large'
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.name}
+						/>
 					</Form.Item>
 					<Form.Item
-						name='userName'
-						rules={[
-							{
-								required: true,
-								message: 'Пожалуйста введите свое имя'
-							}
-						]}
-						validateStatus="success" 
+						name='name'
+						help={touched.name && errors.name}
+						validateStatus={!touched.name ? 'success' : errors.name ? 'error' : 'success' }
 						hasFeedback
 					>
-						<Input prefix={<UserOutlined className='auth__icon-input'/>} placeholder="Ваше имя" size='large'/>
+						<Input 
+							prefix={<UserOutlined className='auth__icon-input'/>}
+							placeholder="Ваше имя" 
+							size='large'
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.name}
+						 />
 					</Form.Item>
 					<Form.Item
 						name='password'
-						rules={[
-							{
-								required: true,
-								message: 'Пожалуйста введите свой пароль'
-							}
-						]}
-						validateStatus="success" 
+						help={touched.password && errors.password}
+						validateStatus={!touched.password ? 'success' : errors.password ? 'error' : 'success' }
 						hasFeedback
 					>
-						<Input.Password prefix={<LockOutlined className='auth__icon-input'/>} placeholder="Пароль" size='large'/>
+						<Input.Password 
+							prefix={<LockOutlined className='auth__icon-input'/>} 
+							placeholder="Пароль" 
+							size='large'
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.name}
+						/>
 					</Form.Item>
 					<Form.Item
 						name='repeat_password'
-						rules={[
-							{
-								required: true,
-								message: 'Пожалуйста введите свой пароль'
-							}
-						]}
-						validateStatus="success" 
+						help={touched.repeat_password && errors.repeat_password}
+						validateStatus={!touched.repeat_password ? 'success' : errors.repeat_password ? 'error' : 'success' } 
 						hasFeedback
 					>
-						<Input.Password prefix={<LockOutlined className='auth__icon-input'/>} placeholder="Повторите пароль" size='large'/>
+						<Input.Password
+							prefix={<LockOutlined className='auth__icon-input'/>} 
+							placeholder="Повторите пароль" 
+							size='large'
+							onChange={handleChange}
+							onBlur={handleBlur}
+							value={values.name}
+						/>
 					</Form.Item>
 					<Form.Item>
-						<Button type="primary" htmlType="submit" className="auth__login-form-button">
+						<Button type="primary" htmlType="submit" onClick={handleSubmit} className="auth__login-form-button">
 							 Зарегистрироваться
 						</Button>
 						<Link to='/login' className='auth__register-link'>Войти в аккаунт</Link>
--- react-node-chat/src/modules/RegisterForm/containers/RegisterForm.js
+++ react-node-chat/src/modules/RegisterForm/containers/RegisterForm.js
@@ -1,3 +1,35 @@
+import { withFormik } from 'formik';
 import RegisterForm from '../components/RegisterForm';
 
-export default RegisterForm;
+const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+const regexPassword = /[a-zA-Z0-9]{8,}/;
+
+ export default  withFormik({
+ 
+   validate: values => {
+     const errors = {};
+	 
+     if (!values.email)  errors.email = 'Пожалуйста введите свое email';
+
+	  if(!regexEmail.test(values.email)) errors.email = 'Пожалуйста, введите корректный  email';
+
+	  if(!values.name) errors.name = 'Пожалуйста введите свое имя';
+	  
+	  if(!regexPassword.test(values.password)) errors.password = 'Минимум 8 символов';
+
+	  if(!regexPassword.test(values.repeat_password)) errors.repeat_password = 'Минимум 8 символов';
+
+
+     return errors;
+   },
+ 
+   handleSubmit: (values, { setSubmitting }) => {
+		
+     setTimeout(() => {
+       alert(JSON.stringify(values, null, 2));
+       setSubmitting(false);
+     }, 1000);
+   },
+ 
+   displayName: 'BasicForm',
+ })(RegisterForm);
